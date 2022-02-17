af942d894e81f84b0c517aa0f3637d42aa5a365d
Jared Min <jkymin2@gmail.com>
2022-02-17 11:12:43 -0500

    Fix pre-render error

--- pages/components/Overlay.js
+++ pages/components/Overlay.js
@@ -18,36 +18,37 @@ export default function Overlay(props) {
 
     return(
         <div className="overlay-container">
-            
-            <div className="side-card-container">
-                <div 
-                    className="side-card-header" 
-                    style={teamColors != [] ? {background: teamColors[0], color: teamColors[1]} : {}}
-                >
-                    <h3>{props.full_name}</h3>
-                    <button className="close-overlay-btn" onClick={props.handleOverlayClose} style={teamColors != [] ? {color: teamColors[1]} : {}}>x</button>
-                </div>
-
-                <h3 className="random-game-details">Random Game Details</h3>
-                
-                <div className="side-card-info-container">
-                    <div className="side-card-left">
-                        <h3>Date</h3>
-                        <h3>Home Team</h3>
-                        <h3>Home Team Score</h3>
-                        <h3>Visitor Team</h3>
-                        <h3>Visitor Team Score</h3>
+            {props.randomGame ? 
+                <div className="side-card-container">
+                    <div 
+                        className="side-card-header" 
+                        style={teamColors != [] ? {background: teamColors[0], color: teamColors[1]} : {}}
+                    >
+                        <h3>{props.full_name}</h3>
+                        <button className="close-overlay-btn" onClick={props.handleOverlayClose} style={teamColors != [] ? {color: teamColors[1]} : {}}>x</button>
                     </div>
 
-                    <div className="side-card-right">
-                        <h3>{dateTime[0]}</h3>
-                        <h3>{props.randomGame.home_team.full_name}</h3>
-                        <h3>{props.randomGame.home_team_score}</h3>
-                        <h3>{props.randomGame.visitor_team.full_name}</h3>
-                        <h3>{props.randomGame.visitor_team_score}</h3>
+                    <h3 className="random-game-details">Random Game Details</h3>
+                    
+                    <div className="side-card-info-container">
+                        <div className="side-card-left">
+                            <h3>Date</h3>
+                            <h3>Home Team</h3>
+                            <h3>Home Team Score</h3>
+                            <h3>Visitor Team</h3>
+                            <h3>Visitor Team Score</h3>
+                        </div>
+
+                        <div className="side-card-right">
+                            <h3>{dateTime[0]}</h3>
+                            <h3>{props.randomGame.home_team.full_name}</h3>
+                            <h3>{props.randomGame.home_team_score}</h3>
+                            <h3>{props.randomGame.visitor_team.full_name}</h3>
+                            <h3>{props.randomGame.visitor_team_score}</h3>
+                        </div>
                     </div>
                 </div>
-            </div>
+            : false}
         </div>
     )
 }
